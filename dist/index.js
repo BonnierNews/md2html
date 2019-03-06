0800118ff76f1d6f9af78ed92cdc0eed2bafaac9
Pål Edman <pal.edman@gmail.com>
2019-03-06 15:02:22 +0100

    with dist

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,24 @@ Object.defineProperty(exports, "__esModule", {
 exports.render = render;
 
 function render(markdown) {
-  const FLOWCONTENT = 1;
-  const PHRASINGCONTENT = 2;
-  const LIST = 3;
+  const NSTATE = {
+    PHRASINGCONTENT: 1,
+    FLOWCONTENT: 2,
+    LIST: 3
+  };
+  const CHARS = {
+    WHITE_SPACE_START: 9,
+    WHITE_SPACE_END: 13,
+    SPACE: 32,
+    HASH: 35,
+    STARTPARENTHESES: 40,
+    ENDPARENTHESES: 41,
+    DASH: 45,
+    ASTERISK: 42,
+    STARTBRACKET: 91,
+    ENDBRACKET: 93,
+    UNDERSCORE: 95
+  };
   let markup = "";
   const state = [];
   const len = markdown.length - 1;
@@ -20,19 +35,18 @@ function render(markdown) {
   for (let idx = 0; idx <= len; ++idx) {
     charcode = markdown.charCodeAt(idx);
 
-    if (charcode >= 9 && charcode <= 13) {
-      // ws
+    if (charcode >= CHARS.WHITE_SPACE_START && charcode <= CHARS.WHITE_SPACE_END) {
       previousIndentation = indentation;
       let lookahead = markdown.charCodeAt(idx + 1);
 
-      if (lookahead === 32) {
+      if (lookahead == CHARS.SPACE) {
         ++indentation;
         let offset;
 
         for (offset = idx + 2; offset < len; ++offset) {
           lookahead = markdown.charCodeAt(offset);
 
-          if (lookahead === 32) {
+          if (lookahead == CHARS.SPACE) {
             ++indentation;
             continue;
           }
@@ -45,26 +59,26 @@ function render(markdown) {
         indentation = 0;
       }
 
-      if (nstate === LIST && lookahead !== 45) {
+      if ((nstate & NSTATE.LIST) == NSTATE.LIST && lookahead !== CHARS.DASH) {
         while (state.length) {
-          markup += "</" + state.pop() + ">";
+          markup += state.pop();
         }
 
-        nstate = undefined;
+        nstate = nstate ^ NSTATE.LIST;
         continue;
       }
 
-      if (!nstate || (nstate & FLOWCONTENT) === FLOWCONTENT) continue;
-      markup += "</" + state.pop() + ">" + String.fromCharCode(charcode);
+      if (!nstate || (nstate & NSTATE.FLOWCONTENT) == NSTATE.FLOWCONTENT) continue;
       nstate = undefined;
+      const lastState = state.pop();
+      if (lastState) markup += lastState + String.fromCharCode(charcode);
       continue;
-    } else if (charcode === 32) {
-      // space
+    } else if (charcode == CHARS.SPACE) {
       markup += " ";
       continue;
-    } else if (charcode === 35) {
+    } else if (charcode == CHARS.HASH) {
       // #
-      if (nstate && (nstate & FLOWCONTENT) === 0) {
+      if (nstate && (nstate & NSTATE.FLOWCONTENT) == 0) {
         markup += "#";
         continue;
       }
@@ -76,10 +90,10 @@ function render(markdown) {
       for (offset = idx + 1; offset < len; ++offset) {
         const lookahead = markdown.charCodeAt(offset);
 
-        if (lookahead === 32) {
+        if (lookahead == CHARS.SPACE) {
           valid = true;
           continue;
-        } else if (lookahead === charcode) {
+        } else if (lookahead == charcode) {
           ++size;
           if (size > 6) break;
           continue;
@@ -89,28 +103,27 @@ function render(markdown) {
       }
 
       if (!valid || size > 6) {
-        nstate = PHRASINGCONTENT;
+        nstate = nstate | NSTATE.PHRASINGCONTENT;
         markup += "<p>";
-        state.push("p");
+        state.push("</p>");
         markup += "#";
         continue;
       }
 
       const element = "h" + size;
       idx = offset - 1;
-      nstate = PHRASINGCONTENT;
+      nstate = nstate | NSTATE.PHRASINGCONTENT;
       markup += "<" + element + ">";
-      state.push(element);
+      state.push("</" + element + ">");
       continue;
-    } else if (charcode === 45) {
-      // -
-      if (nstate && (nstate & FLOWCONTENT) === 0) {
+    } else if (charcode == CHARS.DASH) {
+      if (nstate && (nstate & NSTATE.FLOWCONTENT) == 0) {
         markup += "-";
         continue;
-      } else if (markdown.charCodeAt(idx + 1) !== 32) {
-        nstate = PHRASINGCONTENT;
+      } else if (markdown.charCodeAt(idx + 1) !== CHARS.SPACE) {
+        nstate = NSTATE.PHRASINGCONTENT;
         markup += "<p>";
-        state.push("p");
+        state.push("</p>");
         markup += "-";
         continue;
       }
@@ -118,39 +131,38 @@ function render(markdown) {
       ++idx;
       const upState = state[state.length - 1];
 
-      if (upState === "li" && indentation === previousIndentation) {
-        markup += "</" + state.pop() + ">";
-      } else if (upState === "li" && indentation > previousIndentation) {
+      if (upState == "</li>" && indentation == previousIndentation) {
+        markup += state.pop();
+      } else if (upState == "</li>" && indentation > previousIndentation) {
         // start sublist
         markup += "<ul>";
-        state.push("ul");
+        state.push("</ul>");
       } else if (indentation < previousIndentation) {
         // close sublist
-        markup += "</" + state.pop() + ">"; // close last item
+        markup += state.pop(); // close last item
 
-        markup += "</" + state.pop() + ">"; // close sublist
+        markup += state.pop(); // close sublist
 
-        markup += "</" + state.pop() + ">"; // close parent item
-      } else if (upState !== "ul") {
+        markup += state.pop(); // close parent item
+      } else if (upState !== "</ul>") {
         markup += "<ul>";
-        state.push("ul");
+        state.push("</ul>");
       }
 
-      nstate = LIST;
+      nstate = NSTATE.LIST;
       markup += "<li>";
-      state.push("li");
+      state.push("</li>");
       continue;
-    } else if (charcode === 42 || charcode === 95) {
-      // *_
+    } else if (charcode == CHARS.ASTERISK || charcode == CHARS.UNDERSCORE) {
       if (!nstate) {
         markup += "<p>";
-        state.push("p");
-      } else if ((nstate & PHRASINGCONTENT) === 0) {
-        markup += charcode === 42 ? "*" : "_";
+        state.push("</p>");
+      } else if (!(nstate & NSTATE.PHRASINGCONTENT)) {
+        markup += charcode == CHARS.ASTERISK ? "*" : "_";
         continue;
       }
 
-      const sameahead = markdown.charCodeAt(idx + 1) === charcode;
+      const sameahead = markdown.charCodeAt(idx + 1) == charcode;
       let element;
 
       if (sameahead) {
@@ -160,26 +172,57 @@ function render(markdown) {
         element = "i";
       }
 
-      if (state[state.length - 1] === element) {
-        markup += "</" + state.pop() + ">";
+      if (state[state.length - 1] == "</" + element + ">") {
+        markup += state.pop();
         continue;
       }
 
-      nstate = PHRASINGCONTENT;
+      nstate = nstate | NSTATE.PHRASINGCONTENT;
       markup += "<" + element + ">";
-      state.push(element);
+      state.push("</" + element + ">");
+    } else if (charcode == CHARS.STARTBRACKET) {
+      // [
+      markup += "<a href=\"#link\">";
+      state.push("</a>");
+      nstate = nstate | NSTATE.PHRASINGCONTENT;
+      continue;
+    } else if (charcode == CHARS.ENDBRACKET) {
+      // [
+      let link = "",
+          valid;
+      let offset;
+
+      for (offset = idx + 1; offset < len; ++offset) {
+        const lookahead = markdown.charCodeAt(offset);
+
+        if (lookahead == CHARS.STARTPARENTHESES) {
+          valid = true;
+          continue;
+        } else if (valid && lookahead == CHARS.ENDPARENTHESES) {
+          break;
+        } else if (valid) {
+          link += String.fromCharCode(lookahead);
+          continue;
+        }
+
+        break;
+      }
+
+      idx = offset;
+      markup = markup.replace("<a href=\"#link\">", "<a href=\"" + link + "\">") + state.pop();
+      continue;
     } else {
       if (!nstate) {
-        nstate = PHRASINGCONTENT;
+        nstate = NSTATE.PHRASINGCONTENT;
         markup += "<p>";
-        state.push("p");
+        state.push("</p>");
       }
 
       markup += String.fromCharCode(charcode);
     }
   }
 
-  for (let i = state.length - 1; i >= 0; --i) markup += `</${state[i]}>`;
+  for (let i = state.length - 1; i >= 0; --i) markup += state[i];
 
   return markup;
 }
